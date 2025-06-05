cf3aaca28a1ad9a21d67acdd1425f2b4b863d712
sss <planetrenox@protonmail.com>
2025-06-05 15:24:29 -0700

    Bump version to 0.3.0 and add `ontop` method to `$`. Improve code consistency in `index.js`.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-export const $ = (selector) =>
+const $ = (selector) =>
 {
     const el = document.querySelector(selector)
     el.render = (html) => el.innerHTML = html
     el.inject = (html) => el.insertAdjacentHTML('beforeend', html)
+    el.ontop = (html) => el.insertAdjacentHTML('beforebegin', html)
     return el;
 };
 
-export const $$ = (selector) => return document.querySelectorAll(selector)
-export const _ = msg => console.log(msg); // _('hello')
+const $$ = (selector) => { return document.querySelectorAll(selector)}
+const _ = msg => console.log(msg); // _('hello')
